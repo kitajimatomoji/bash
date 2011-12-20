43282283d5bbcf56c827b62a69ed1919483f3d59
Tomoji Kitajima <github@kitajima.asia>
2011-12-20 15:32:20 +0900

    Signed-off-by: Tomoji Kitajima <github@kitajima.asia>

--- alivemonitor.sh
+++ alivemonitor.sh
@@ -1,6 +1,31 @@
+MDATE=`date +%Y%m%d`
+
+# RAID 状態 ###################################################################
+/usr/local/bin/tw_cli info c0 > /home/admin/raid_status/${MDATE}
+chmod 666 /home/admin/raid_status/${MDATE}
+
 # 不正アクセスIPアドレスを拒否設定に追加する
 cat /var/log/secure|grep "Invalid user"|awk '{print $NF}' FS=":"|uniq|while read line; do
   if [ `grep -c $line /etc/hosts.deny` == 0 ]; then
     echo "all: $line" >> /etc/hosts.deny
   fi
 done
+
+# 負荷 ########################################################################
+if [ `free|awk '{if((NR==2 && $4<1000) || (NR==4 && $4<8000)) print $4}'|wc -l` != 0 ]; then
+  free|mail alert@example.com -s 'WARNING - Main-memory is almost full'
+fi
+
+# MySQL #######################################################################
+mystat=`mysqladmin ping -uuser -ppasswd`
+
+if [ "'${mystat}'" != "'mysqld is alive'" ]; then
+  echo ${mystat}|mail alert@example.com -s 'WARNING - MySQL is not alive'
+fi
+
+slaveio=`echo 'show slave status\G' | mysql -uuser -ppasswd | grep 'Slave_IO_Running' | awk '{print $NF}' FS=':' | sed 's/ //'`
+slavesql=`echo 'show slave status\G' | mysql -uuser -ppasswd | grep 'Slave_SQL_Running' | awk '{print $NF}' FS=':' | sed 's/ //'`
+if [ "'${slaveio}'" != "'Yes'" ] || [ "'${slavesql}'" != "'Yes'" ]; then
+  mail alert@example.com -s 'WARNING - MySQL Slave is not running'
+fi
+
